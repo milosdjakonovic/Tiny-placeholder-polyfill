8b3a61d9516e3dbe1953513c497d89b6e9f27452
Miloš Đakonović <milos@djakonovic.net>
2016-10-20 07:52:33 +0200

    Refactoring...

--- README.md
+++ README.md
@@ -2,18 +2,7 @@ Tiny placeholder polyfill
 =========================
 
 Placeholder polyfill in less than 1KB! 
-NO jQuery dependency, but compatible with jQuery.
-
 
 Usage:
 
-simply include script: <script src="path/to/js/tpp.min.js"></script>  
-
-Case jQuery present:
-Tiny placeholder polyfill will release tpp variable in global namespace (window.tpp), create jQuery.fn.tpp so you can intialize
-placeholder polyfill on selected element e.g. $('input,textarea').tpp() and automatically initialize polyfill on all found input and
-textarea elements on document load event.
-
-Case jQuery not present:
-Tiny placeholder polyfill will release tpp variable in global namespace (window.tpp) so you can pass selected element as function
-argument e.g. tpp(document.getElementById('#myInput'))
+simply include script: `<script src="path/to/js/tpp.js"></script>`
--- js/tpp.js
+++ js/tpp.js
@@ -1,58 +1,97 @@
+(function(w,d,$, p){
+
 /**
-*
-*  Tiny placeholder polyfill
-*
-*
-*
-*/
-
-            (function(a, b) {
-                a.tpp = function(elem) {
-                    if (document.createElement('input').placeholder != undefined) return;
-                    var addEvent = function addEvent(obj, type, fn) {
-                        if (obj.attachEvent) {
-                            obj['e' + type + fn] = fn;
-                            obj[type + fn] = function() {
-                                obj['e' + type + fn](window.event);
-                            }
-                            obj.attachEvent('on' + type, obj[type + fn]);
-                        } else
-                            obj.addEventListener(type, fn, false);
-                    };
-                    var register = {
-                        typed: false
-                    };
-                    elem.value = elem.getAttribute('placeholder');
-
-                    addEvent(elem, 'keyup', function() {
-                        register.typed = true;
-                    });
-
-                    addEvent(elem, 'focus', function() {
-                        if (register.typed === false || elem.value === elem.getAttribute('placeholder')) {
-                            elem.value = ''
-                        }
-
-                    });
-                    addEvent(elem, 'blur', function() {
-                        if (register.typed === false || elem.value === '') {
-                            elem.value = elem.getAttribute('placeholder')
-                        }
-                    });
+ * If browser supports placeholder natively,
+ * no need to do anything
+ */
+if (d.createElement('input')[p] != undefined) 
+    return;
 
 
-                }
+/**
+ * addEventListener-attachEvent
+ * abstraction
+ */
+var addEvent = function(obj, type, fn) {
+    if (obj.attachEvent) {
+        obj['e' + type + fn] = fn;
+        obj[type + fn] = function() {
+            obj['e' + type + fn](window.event);
+        }
+        obj.attachEvent('on' + type, obj[type + fn]);
+    } else
+        obj.addEventListener(type, fn, false);
+},
+/**
+ * Simple array iterator
+ */
+_a = function(array,method){
+      var templ=array.length;
+      for(var i=0;i<templ;i++){
+        method.call(this, i, array[i] );
+      }
+},
+/**
+ * Smallest DOM Ready ever enhanced
+ * https://gist.github.com/milosdjakonovic/9814b725b86b0e862e8201124844d791
+ */
+r = function(f){
+        /in/.test(d.readyState)?setTimeout(function(){r(f)},9):f()    
+},
 
-                if (typeof(b) != 'undefined') {
-                    b.fn.tpp = function() {
-                        a.tpp(this.get(0));
-                        return this;
-                    }
-                    b(function() {
-                        b('input,textarea').tpp();
-                    });
+/**
+ * Tiny placeholder polyfill
+ * @param el - HTMLElement -> input or textarea
+ * @return el 
+ * 
+ */
+tpp = function(el){
+    var register = {typed:true};
+    el.value = el.getAttribute(p);
 
-                }
+    // avoid if it's already done 
+    if(el.getAttribute('data-tpp-done'))
+        return;
 
+    addEvent(el, 'keyup', function() {
+        register.typed = true
+    });
 
-            })(window, jQuery);
+    // empty value on focus, when needed
+    addEvent(el, 'focus', function() {
+        if (register.typed === false || el.value === el.getAttribute(p)) {
+            el.value = ''
+        }
+    });
+    // restore placeholder attr to value,
+    // when needed
+    addEvent(el, 'blur', function() {
+        if (register.typed === false || el.value === '' ) {
+            el.value = el.getAttribute(p)
+        }
+    });
+
+    //mark as done for this el
+    el.setAttribute('data-tpp-done', '1');
+
+    return el;
+},
+
+all = function(){
+    var inputs = d.getElementsByTagName('input'   ),
+        tareas = d.getElementsByTagName('textarea');
+    _a(inputs, function(i,ele){
+        tpp(ele);
+    });
+    _a(tareas, function(i,ele){
+        tpp(ele);
+    });        
+};
+
+tpp.all = all; 
+
+r(all);
+
+w.Tpp = tpp;
+
+})(window,document,window.jQuery, 'placeholder');
